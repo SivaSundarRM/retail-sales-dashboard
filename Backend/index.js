6fbce00dd5a480afef3d4e6b8668ea3b434ce732
SivaSundarRM <sivaramachandran143@gmail.com>
2025-12-09 10:50:55 +0530

    Refactor CORS and MongoDB connection handling

--- Backend/index.js
+++ Backend/index.js
@@ -4,16 +4,23 @@ import cors from "cors";
 import salesRoutes from "./routes/salesRoutes.js";
 
 const app = express();
-app.use(cors());
+
+app.use(cors({
+  origin: "https://retail-sales-dashboard-1-lnyg.onrender.com", 
+  methods: ["GET"],
+  allowedHeaders: ["Content-Type"],
+}));
+
 app.use(express.json());
 
 mongoose
-  .connect(
-    "mongodb+srv://Siva:nZcBht17rwymWkil@cluster0.pyx7cj1.mongodb.net/SalesDB?retryWrites=true&w=majority&appName=Cluster0"
-  )
-  .then(() => console.log("✅ MongoDB Atlas Connected"))
+  .connect("mongodb+srv://Siva:nZcBht17rwymWkil@cluster0.pyx7cj1.mongodb.net/SalesDB?retryWrites=true&w=majority&appName=Cluster0")
+  .then(() => console.log("MongoDB Atlas Connected"))
   .catch((err) => console.log(" MongoDB Connection Error:", err));
 
 app.use("/api", salesRoutes);
 
-app.listen(5000, () => console.log(" Backend running on port 5000"));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () =>
+  console.log(`Backend running on port ${PORT}`)
+);
