dd42025d2cd08de4504107b9c8be31315ed50266
SivaSundarRM <sivaramachandran143@gmail.com>
2025-12-17 12:45:07 +0530

    Modify to include default page parameter

--- Frontend/src/services/api.js
+++ Frontend/src/services/api.js
@@ -3,8 +3,13 @@ import axios from "axios";
 const API_URL = "https://retail-sales-dashboard-bhpd.onrender.com/api/sales";
 
 export default {
-  getSales: async (params) => {
-    const res = await axios.get(API_URL, { params });
+  getSales: async (params = {}) => {
+    const res = await axios.get(API_URL, {
+      params: {
+        page: 1,
+        ...params
+      }
+    });
     return res.data;
   }
 };
